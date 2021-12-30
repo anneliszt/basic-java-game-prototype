f9901aa511571e47f660e1811d5cf503222218b1
Ann <71485613+anneliszt@users.noreply.github.com>
2021-12-30 18:22:22 +0800

    Title update

--- src/com/tutorial/src/MainClass.java
+++ src/com/tutorial/src/MainClass.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class MainClass {
 	
 	public static final int WIDTH = 640, HEIGHT = 480;
-	public static final String TITLE = "Tutorial";
+	public static final String TITLE = "Basic Game Window";
 	
 	public static void main(String args[]) {
 		
